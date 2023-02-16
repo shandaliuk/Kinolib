7e168f406c0dfc920fc9480197b540f405a3e3a0
Kyrylo <shandaliuk3019@gmail.com>
2023-02-16 16:27:41 +0200

    Added modal window

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,9 +1,20 @@
+import { useState } from 'react';
 import { getGenres, getYear, normalizeTitle } from 'services/serviceApi';
+import { MovieModal } from 'components/Modal/Modal';
 import { Image, MovieTitle, InfoWrapper } from './Movie.styled';
 
 export const Movie = ({ title, poster, date, genreIds }) => {
+  const [isModalOpened, setModalOpening] = useState(false);
+
+  const handleOpening = event => {
+    event.preventDefault();
+    setModalOpening(true);
+  };
+
+  const handleClosure = () => setModalOpening(false);
+
   return (
-    <>
+    <a href="/" onClick={handleOpening}>
       <Image
         src={`https://image.tmdb.org/t/p/w500${poster}`}
         alt={`Movie: ${title}`}
@@ -13,6 +24,13 @@ export const Movie = ({ title, poster, date, genreIds }) => {
         <ul>{getGenres(genreIds)} </ul>
         <p>&nbsp;| {getYear(date)}</p>
       </InfoWrapper>
-    </>
+      {isModalOpened && (
+        <MovieModal
+          isOpened={isModalOpened}
+          onOpening={handleOpening}
+          onClosure={handleClosure}
+        />
+      )}
+    </a>
   );
 };
